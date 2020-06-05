31631891df1812a0a52d49ef1b3ef02da701aeec
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-06-06 01:36:16 +0700

    Add project files.

---
+++ Daftar Teman 2/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private listNama As New List(Of String)
+
+    Private Sub tombolTambah_Click(sender As Object, e As EventArgs) Handles tombolTambah.Click
+        'menambahkan nama ke List
+        listNama.Add(teksNama.Text)
+
+        'membersihkan kotak teks
+        teksNama.Clear()
+        teksNama.Focus()
+    End Sub
+
+    Private Sub tombolTampil_Click(sender As Object, e As EventArgs) Handles tombolTampil.Click
+        'menampilkan semua nama pada kotak list
+        Dim intIndeks As Integer = 0
+        Dim strNama As String
+
+        For intIndeks = 0 To listNama.Count - 1
+            strNama = listNama(intIndeks)
+            listTeman.Items.Add(strNama)
+        Next
+    End Sub
+End Class
